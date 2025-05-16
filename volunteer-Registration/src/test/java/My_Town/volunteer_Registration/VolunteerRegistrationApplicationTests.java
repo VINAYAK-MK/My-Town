e47e4adcf9d2ce983d7ea0929e93dcc870241606
VINAYAK-MK <vinayakmkulkarni2001@gmail.com>
2025-05-16 17:29:21 +0530

    volunteer-Registration microservice added

---
+++ volunteer-Registration/src/test/java/My_Town/volunteer_Registration/VolunteerRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package My_Town.volunteer_Registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
